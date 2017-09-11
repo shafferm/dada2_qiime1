db02d0bddea014bc8a3e2f16966eea942015e990
shafferm <michael.shaffer@ucdenver.edu>
2017-09-11 15:09:41 -0600

    Got rid of r installer

--- setup.py
+++ setup.py
@@ -7,26 +7,26 @@ __author__ = 'shafferm'
 __version__ = '0.1.1'
 
 
-class CustomInstallCommand(install):
-    """Customized setuptools install command."""
-    def run(self):
-        # load r functions
-        r_install_packages = robjects.r['install.packages']
-        r_source = robjects.r['source']
-
-        # setup bioconductor
-        r_source("http://bioconductor.org/biocLite.R")
-        r_biocLite = robjects.r['biocLite']
-
-        # install argparser
-        r_install_packages("argparser", repos="http://cran.us.r-project.org")
-
-        # install dada2
-        r_biocLite('dada2')
-        install.run(self)
+# class CustomInstallCommand(install):
+#     """Customized setuptools install command."""
+#     def run(self):
+#         # load r functions
+#         r_install_packages = robjects.r['install.packages']
+#         r_source = robjects.r['source']
+#
+#         # setup bioconductor
+#         r_source("http://bioconductor.org/biocLite.R")
+#         r_biocLite = robjects.r['biocLite']
+#
+#         # install argparser
+#         r_install_packages("argparser", repos="http://cran.us.r-project.org")
+#
+#         # install dada2
+#         r_biocLite('dada2')
+#         install.run(self)
 
 setup(
-    cmdclass={'install': CustomInstallCommand},
+    # cmdclass={'install': CustomInstallCommand},
     name="dada2_qiime1",
     version=__version__,
     install_requires=['rpy2', 'biom-format', 'numpy'],
